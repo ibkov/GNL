35ac7ff394398dc582cb5a52ad12c2dfe66e7bb6
burswyck <ibkov@yandex.ru>
2020-12-10 19:28:47 +0300

    last

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
 	int fd;
 	char *a;
 
-	fd = open("/Users/burswyck/Desktop/GNL/123.txt", O_RDONLY);
+	fd = open("/Users/burswyck/Desktop/GNL/user_output_buff_16_one_big_fat_line.txt", O_RDONLY);
 	while (get_next_line(fd, &a)) {
 		printf("%s\n", a);
 		free(a);
