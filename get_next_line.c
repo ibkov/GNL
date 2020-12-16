362fb717e3e04a6156c91abb86ed0860ee115b86
ibkov <34207589+ibkov@users.noreply.github.com>
2020-12-17 00:16:57 +0300

    add change last

--- get_next_line.c
+++ get_next_line.c
@@ -6,58 +6,96 @@
 /*   By: burswyck <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 17:53:28 by burswyck          #+#    #+#             */
-/*   Updated: 2020/11/11 17:53:32 by burswyck         ###   ########.fr       */
+/*   Updated: 2020/12/12 14:15:59 by burswyck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *ft_check_last(char *last, char **line)
+void	ft_strcpy(char *restrict dst,\
+const char *restrict src)
 {
-	char *p_n;
+	if (!dst && !src)
+		return ;
+	while (*src)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+	}
+	*dst = '\0';
+}
+
+char	*ft_check_last(char *last, char **line)
+{
+	char	*p_n;
 
 	p_n = NULL;
 	if (last)
 		if ((p_n = ft_strchr(last, '\n')))
 		{
 			*p_n = '\0';
-			*line = ft_strdup(last);
+			if (!(*line = ft_strdup(last)))
+				return (0);
 			p_n++;
 			ft_strcpy(last, p_n);
 		}
 		else
 		{
-			*line = ft_strdup(last);
+			if (!(*line = ft_strdup(last)))
+				return (0);
 			*last = '\0';
 		}
 	else
-		*line = ft_calloc(1,1);
+		if (!(ft_calloc(line)))
+			return (0);
 	return (p_n);
 }
 
-int get_next_line(int fd, char **line) {
-	char buffer[BUFFER_SIZE + 1];
-	int read_number;
-	char *p_n;
-	static char *last;
+int	free_join_line(char **line, char *buffer)
+{
+	char	*temp;
+
+	temp = *line;
+	if (!(*line = ft_strjoin(*line, buffer)))
+		return (0);
+	free(temp);
+	return (1);
+}
+
+char *free_mem(char *p_n, char *last)
+{
 	char *temp;
+
+	*p_n = '\0';
+	temp = last;
+	if (!(last = ft_strdup(++p_n)))
+		return (0);
+	free(temp);
+	return (last);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	char			buffer[((BUFFER_SIZE) < 1) ? (1) : (BUFFER_SIZE + 1)];
+	int				rn;
+	char			*p_n;
+	static char		*last;
 
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
 	p_n = ft_check_last(last, line);
-	while (!p_n && (read_number = read(fd, buffer, BUFFER_SIZE))) {
-		buffer[read_number] = '\0';
-		if ((p_n = ft_strchr(buffer, '\n'))) {
-			*p_n = '\0';
-			p_n++;
-			free(last);
-			last = ft_strdup(p_n);
+	while (!p_n && (rn = read(fd, buffer, BUFFER_SIZE)))
+	{
+		if (rn == -1)
+			return (-1);
+		buffer[rn] = '\0';
+		if ((p_n = ft_strchr(buffer, '\n')))
+		{
+			last = free_mem(p_n, last);
 		}
-		temp = *line;
-		*line = ft_strjoin(*line, buffer);
-		free(temp);
+		if (!(free_join_line(line, buffer)))
+			return (-1);
 	}
-	if (p_n)
-		return (1);
-	return (0);
+	return (p_n) ? 1 : 0;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,26 @@
 /*   By: burswyck <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 17:53:56 by burswyck          #+#    #+#             */
-/*   Updated: 2020/11/11 17:54:02 by burswyck         ###   ########.fr       */
+/*   Updated: 2020/12/12 14:14:06 by burswyck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GNL_GET_NEXT_LINE_H
-#define GNL_GET_NEXT_LINE_H
+# define GNL_GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
-int get_next_line(int fd, char **line);
-char    *ft_strdup(const char *s1);
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_strchr(const char *string, int symbol);
-int    ft_strlen(const char *str);
-void    ft_bzero(void *s, size_t n);
-void    *ft_calloc(size_t count, size_t size);
-void    ft_strcpy(char *restrict dst,\
+int		get_next_line(int fd, char **line);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int symbol);
+int		ft_strlen(const char *str);
+int		ft_calloc(char **line);
+void	ft_strcpy(char *restrict dst,\
 const char *restrict src);
 
-#endif //GNL_GET_NEXT_LINE_H
+#endif
